005ccc9732ea1b1bcfbeb2a97aa8a2d3da4667e4
Kristian <kristian.lissvall@knowit.se>
2022-12-19 14:00:00 +0100

    test msal library

--- Download-onedrive-file/__init__.py
+++ Download-onedrive-file/__init__.py
@@ -4,9 +4,18 @@ import logging
 import base64
 import requests
 import shutil
+import json
+import msal
+import jwt
+import pandas as pd
 from azure.storage.blob import BlobServiceClient, ContainerClient
 import azure.functions as func
 
+accessToken = None 
+requestHeaders = None 
+tokenExpiry = None 
+queryResults = None 
+graphURI = 'https://graph.microsoft.com'
 
 def main(mytimer: func.TimerRequest) -> None:
     utc_timestamp = datetime.utcnow().replace(
@@ -14,6 +23,7 @@ def main(mytimer: func.TimerRequest) -> None:
 
     if mytimer.past_due:
         logging.info('The timer is past due!')
+
     # Create the BlobServiceClient object
     creds=os.getenv("ACCOUNT_KEY")
     account_url = "https://lakeciehknalis.blob.core.windows.net/"
@@ -28,20 +38,35 @@ def main(mytimer: func.TimerRequest) -> None:
     # Create the container
     if not container.exists():
         blob_service_client.create_container(container_name) 
-    resultUrl = create_onedrive_directdownload("https://knowit.sharepoint.com/:x:/r/sites/org-165-Internal/Shared%20Documents/Semesterlista%20SOL%202021%20-%20Copy.xlsx?d=w49625d1c2dbe4e44a66490f7cc6da5ef&csf=1&web=1&e=nSsj4w")
-    headers = {'content-type': 'application/octet-stream'}
-    # download_file(resultUrl) to local storage first? 
-    r = requests.get(resultUrl, headers=headers, stream=True)
-    result_File = r.raw.read()
-    # with open(path, 'w') as f:
-    #     f.write(img)
-    # Create a blob client using the local file name as the name for the blob 
-    now = datetime.now()
-    blob_name = now.strftime("employees_copy%m%d%Y-%H:%M:%S.xlsx")
-    blob_client = blob_service_client.get_blob_client(container=container_name, blob=blob_name)
+
+    # Auth
+    msgraph_auth()
+
+    # Query
+    queryResults = msgraph_request(graphURI +'/v1.0/users',requestHeaders)
+
+    # Results to Dataframe
+    try:
+        df = pd.read_json(json.dumps(queryResults['value']))
+        # set ID column as index
+        df = df.set_index('id')
+        print(str(df['displayName'] + " " + df['mail']))
+    except:
+        print(json.dumps(queryResults, indent=2))
+
+    # resultUrl = create_onedrive_directdownload("https://knowit.sharepoint.com/:x:/r/sites/org-165-Internal/Shared%20Documents/Semesterlista%20SOL%202021%20-%20Copy.xlsx?d=w49625d1c2dbe4e44a66490f7cc6da5ef&csf=1&web=1&e=nSsj4w")
+    # headers = {'content-type': 'application/octet-stream'}
+
+    # r = requests.get(resultUrl, headers=headers, stream=True)
+    # result_File = r.raw.read()
+
+    # # Create a blob client using the local file name as the name for the blob 
+    # now = datetime.now()
+    # blob_name = now.strftime("employees_copy%m%d%Y-%H:%M:%S.xlsx")
+    # blob_client = blob_service_client.get_blob_client(container=container_name, blob=blob_name)
     
-    # Upload the created file
-    blob_client.upload_blob(result_File)
+    # # Upload the created file
+    # blob_client.upload_blob(result_File)
 
     logging.info('Python timer trigger function ran at %s', utc_timestamp)
 
@@ -59,3 +84,47 @@ def create_onedrive_directdownload (onedrive_link):
     data_bytes64_String = data_bytes64.decode('utf-8').replace('/','_').replace('+','-').rstrip("=")
     resultUrl = f"https://api.onedrive.com/v1.0/shares/u!{data_bytes64_String}/root/content"
     return resultUrl
+
+def msgraph_auth():
+    global accessToken
+    global requestHeaders
+    global tokenExpiry
+    tenantID = 'e34d5777-07b1-4c5b-bbea-068c4139108c'
+    authority = 'https://login.microsoftonline.com/' + tenantID
+    clientID = '4aa12be5-1830-4ed1-8f4a-3b626ed6b303'
+    clientSecret = 'Ljz8Q~ETSbGaR_RhxHgAzQ2pzJE_TFyjjMwVEasl'
+    scope = ['https://graph.microsoft.com/.default','files.readwrite']
+
+    app = msal.ConfidentialClientApplication(clientID, authority=authority, client_credential = clientSecret)
+
+    try:
+        accessToken = app.acquire_token_silent(scope, account=None)
+        if not accessToken:
+            try:
+                accessToken = app.acquire_token_for_client(scopes=scope)
+                if accessToken['access_token']:
+                    print('New access token retreived....')
+                    requestHeaders = {'Authorization': 'Bearer ' + accessToken['access_token']}
+                else:
+                    print('Error aquiring authorization token. Check your tenantID, clientID and clientSecret.')
+            except:
+                pass 
+        else:
+            print('Token retreived from MSAL Cache....')
+
+        decodedAccessToken = jwt.decode(accessToken['access_token'], verify=False)
+        accessTokenFormatted = json.dumps(decodedAccessToken, indent=2)
+        print('Decoded Access Token')
+        print(accessTokenFormatted)
+
+        # Token Expiry
+        tokenExpiry = datetime.fromtimestamp(int(decodedAccessToken['exp']))
+        print('Token Expires at: ' + str(tokenExpiry))
+        return
+    except Exception as err:
+        print(err)
+
+def msgraph_request(resource,requestHeaders):
+    # Request
+    results = requests.get(resource, headers=requestHeaders).json()
+    return results
